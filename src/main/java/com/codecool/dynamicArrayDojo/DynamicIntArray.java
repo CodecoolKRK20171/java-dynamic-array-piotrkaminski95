07e3b4d592deb0c0b5c16e795aff17db3d009692
piotrkaminski95 <pkaminki95@gmail.com>
2018-11-07 21:52:18 +0100

    implement add(), refactor toString

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -12,20 +12,33 @@ public class DynamicIntArray {
         this.arr = new int[size];
     }
 
-    public void add(int num) {}
+    public void add(int num) {
+        this.arr = increaseArr();
+        this.arr[this.arr.length - 1] = num;
+    }
+
+    private int[] increaseArr() {
+        int[] newArr = new int[this.arr.length + 1];
+
+        for (int i = 0; i < this.arr.length; i++ ) {
+            newArr[i] = this.arr[i];
+        }
+        return newArr;
+    }
 
     public void remove(int index) {}
 
     public void insert(int index, int num) {}
 
     @Override
-    public String toString() {
+    public String toString() { return arrToString(); }
+
+    private String arrToString() {
         String toReturn = "";
 
         for (int n : this.arr) {
-            toReturn = String.format("%s%s", toReturn, Integer.toString(n));
+            toReturn = String.format("%s %s", toReturn, Integer.toString(n));
         }
-
         return toReturn;
     }
 }
