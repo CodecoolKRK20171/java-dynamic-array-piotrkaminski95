736bb828c7f0ed237d3bac78f58866a9b78287d2
piotrkaminski95 <pkaminki95@gmail.com>
2018-11-07 21:12:19 +0100

    add required methods

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,15 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private int[] arr;
 
+    public DynamicIntArray() {}
+
+    public DynamicIntArray(int size) {}
+
+    public void add(int num) {}
+
+    public void remove(int index) {}
+
+    public void insert(int index, int num) {}
 }
